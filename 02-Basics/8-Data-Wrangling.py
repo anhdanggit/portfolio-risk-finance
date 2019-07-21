ff1c9d9c901f6f22d9656c768944e32c7ca4176c
anhdanggit <maianhdang.ftu@gmail.com>
2019-07-22 00:41:19 +0100

    Create 8-Data-Wrangling.py

---
+++ 02-Basics/8-Data-Wrangling.py
@@ -0,0 +1,22 @@
+'''
+#FILE: DATA WRANGLING
+Project: portfolio-risk-finance
+-------------------
+By: Anh Dang
+Date: 2019-07-21
+Description:
+Overview some common tricks in join, combine, and reshapre
+'''
+
+## Import modules
+import pandas as pd
+import numpy as np
+import os
+import scipy as sp
+import matplotlib.pyplot as plt
+
+## 1 - Indexing -------
+
+## 2 - Combine |= Merge -------
+
+## 3 - Reshapre + Pivoting -------
